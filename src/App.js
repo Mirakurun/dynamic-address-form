cf35007bf4077b37c4f690cc58ca329fc848cb0f
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-08-06 05:59:56 -0700

    Update form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
 import AddressForm from './components/AddressForm';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
--- src/components/AddressForm.js
+++ src/components/AddressForm.js
@@ -12,35 +12,81 @@ import {
 } from '@material-ui/core';
 import AddressField from '../data/addressfield.json';
 
-function Address(number) {
-  return (
-    <FormControl fullWidth>
-      <TextField id={`address${number}`} label={`Address Line ${number}`} />
-    </FormControl>
-  );
-}
-
-function LocalityList({ localities }) {
-  const breakpoint = 12 / localities.length;
-  const listItems = localities.map((locality, index) => (
-    <Grid item key={index} xs={breakpoint}>
-      locality
-    </Grid>
-  ));
-
-  return listItems;
-}
-
-LocalityList.propTypes = {
-  localities: PropTypes.arrayOf(PropTypes.object),
-};
-
-LocalityList.defaultProps = {
-  localities: [],
-};
-
 const AddressForm = () => {
   const [country, setCountry] = useState(AddressField.options[0]);
+  const [administrativeArea, setAdministrativeArea] = useState('');
+
+  function Address(number) {
+    return (
+      <FormControl fullWidth>
+        <TextField id={`address${number}`} label={`Address Line ${number}`} />
+      </FormControl>
+    );
+  }
+
+  function Locality({ locality }) {
+    const keys = Object.keys(locality);
+
+    return (
+      <FormControl fullWidth>
+        {locality[keys[0]].options ? (
+          <>
+            <InputLabel id={`${locality[keys[0]]}-label`}>
+              {locality[keys[0]].label}
+            </InputLabel>
+            <Select
+              id={`${locality[keys[0]]}-select`}
+              labelId={`${locality[keys[0]]}-label`}
+              value={administrativeArea}
+              onChange={(e) => setAdministrativeArea(e.target.value)}
+            >
+              {locality[keys[0]].options.map((option) => {
+                const optionKeys = Object.keys(option);
+                const optionValues = Object.values(option);
+                if (optionKeys[0] === '') {
+                  return null;
+                }
+                return (
+                  <MenuItem key={optionKeys[0]} value={optionKeys[0]}>
+                    {optionValues[0]}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </>
+        ) : (
+          <TextField id={keys[0]} label={locality[keys[0]].label} />
+        )}
+      </FormControl>
+    );
+  }
+
+  Locality.propTypes = {
+    locality: PropTypes.objectOf(PropTypes.object),
+  };
+
+  Locality.defaultProps = {
+    locality: {},
+  };
+
+  function LocalityList({ localities }) {
+    const breakpoint = 12 / localities.length;
+    const listItems = localities.map((locality, index) => (
+      <Grid item key={index} xs={breakpoint}>
+        <Locality locality={locality} />
+      </Grid>
+    ));
+
+    return listItems;
+  }
+
+  LocalityList.propTypes = {
+    localities: PropTypes.arrayOf(PropTypes.object),
+  };
+
+  LocalityList.defaultProps = {
+    localities: [],
+  };
 
   const handleCountryChange = (event) => {
     setCountry(event.target.value);
